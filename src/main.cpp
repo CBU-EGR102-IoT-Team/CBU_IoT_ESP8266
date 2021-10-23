99bae4ed9f5dd72b5f0e0b0026b75763b4281b3f
Zach Bohn <zach.bohn117@gmail.com>
2021-10-23 08:55:14 -0700

    Added Cert Control Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,96 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <FS.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
-//WIFI settings
+// WIFI settings
 const char *ssid = "CBU";
 const char *password = "";
 
-//MQTT paramaters
-const char *mqtt_server = "broker.hivemq.com"; //Change to match MQTT broker in use
-size_t mqtt_port = 1883;
-//MQTT Topic Structure: EGR102TA/ABox#/Sensor
-String baseTopic = "cbuta";
-String baseTopic1 = baseTopic + "0/"; //baseTopic + id
+// MQTT paramaters
+const char *mqtt_server = "a1lxf7uv741fru-ats.iot.us-west-1.amazonaws.com"; // Change to match MQTT broker/AWS Endpoint in use
+size_t mqtt_port = 8883;
+// MQTT Topic Structure: EGR102TA/ABox#/Sensor
+String baseTopic = "EGR102TA/";
+String baseTopic1 = baseTopic + "0/"; // baseTopic + id
 
-//functions
+// functions
+
 void wifi_setup();
 void MQTTinit();
 void MQTTreconnect();
 void callback(char *topic, byte *payload, unsigned int length);
 void mqttPost(String topic, String data);
 
-//objects
-WiFiClient espClient;
-PubSubClient MQTTclient;
+// objects
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
+WiFiClientSecure espClient;
+PubSubClient MQTTclient(mqtt_server, mqtt_port, callback, espClient);
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  pinMode(LED_BUILTIN, OUTPUT);
   wifi_setup();
-  
-  MQTTinit(); //init MQTT
-  if (!MQTTclient.connected()) MQTTreconnect();
+  delay(1000);
+
+  if (!SPIFFS.begin())
+  {
+    Serial.println("Failed to mount filesystem");
+    ESP.restart();
+  }
+
+  Serial.print("Heap: ");
+  Serial.println(ESP.getFreeHeap());
+  // Load certificate file
+  File cert = SPIFFS.open("/cert.der", "r"); // replace cert.crt eith your uploaded file name
+  if (!cert)
+  {
+    Serial.println("Failed to open cert file");
+  }
+  else
+    Serial.println("Success to open cert file");
+  delay(1000);
+  if (espClient.loadCertificate(cert))
+    Serial.println("cert loaded");
+  else
+    Serial.println("cert not loaded");
+  // Load private key file
+  File private_key = SPIFFS.open("/private.der", "r"); // replace private eith your uploaded file name
+  if (!private_key)
+  {
+    Serial.println("Failed to open private cert file");
+  }
+  else
+    Serial.println("Success to open private cert file");
+  delay(1000);
+  if (espClient.loadPrivateKey(private_key))
+    Serial.println("private key loaded");
+  else
+    Serial.println("private key not loaded");
+  // Load CA file
+  File ca = SPIFFS.open("/ca.der", "r"); // replace ca eith your uploaded file name
+  if (!ca)
+  {
+    Serial.println("Failed to open ca ");
+  }
+  else
+    Serial.println("Success to open ca");
+  delay(1000);
+  if (espClient.loadCACert(ca))
+    Serial.println("ca loaded");
+  else
+    Serial.println("ca failed");
+  Serial.print("Heap: ");
+  Serial.println(ESP.getFreeHeap());
+
+  MQTTinit(); // init MQTT
+  if (!MQTTclient.connected())
+    MQTTreconnect();
 }
 
 // Loop Monitor variables
@@ -39,94 +100,103 @@ int serialPRequest = 0;
 int loopCOunter = 0;
 String input = "";
 
-void loop() {
-  //int input;
+void loop()
+{
+  // int input;
 
-  //if serial is available print it back out to serial
-  if(serialRequest != 0){
-    if(Serial.available() > 0){
-      //Serial.println(Serial.read());
+  // if serial is available print it back out to serial
+  if (serialRequest != 0)
+  {
+    if (Serial.available() > 0)
+    {
+      // Serial.println(Serial.read());
       input += (char)Serial.read();
       serialPState = true;
-    } else serialPState = false;
+    }
+    else
+      serialPState = false;
 
-    //if the request was recieved
-    if(serialPState == true && Serial.available() == 0){
-      if(serialRequest == 1) //ID
+    // if the request was recieved
+    if (serialPState == true && Serial.available() == 0)
+    {
+      if (serialRequest == 1) // ID
       {
         mqttPost("cbuta/ID", input);
       }
-      if(serialRequest == 2) //Volt1
+      if (serialRequest == 2) // Volt1
       {
         mqttPost("cbuta/Volt1", input);
       }
-      if(serialRequest == 3) //Volt2
+      if (serialRequest == 3) // Volt2
       {
         mqttPost("cbuta/Volt2", input);
       }
-      if(serialRequest == 4) //Light1
+      if (serialRequest == 4) // Light1
       {
         mqttPost("cbuta/Light1", input);
       }
-      if(serialRequest == 5) //Light2
+      if (serialRequest == 5) // Light2
       {
         mqttPost("cbuta/Light2", input);
       }
-      if(serialRequest == 6) //Distance1
+      if (serialRequest == 6) // Distance1
       {
         mqttPost("cbuta/Distance1", input);
       }
-      if(serialRequest == 7) //Distance2
+      if (serialRequest == 7) // Distance2
       {
         mqttPost("cbuta/Distance2", input);
       }
 
       serialPRequest = serialRequest;
-      serialRequest = 0; //this frees up the loop
+      serialRequest = 0; // this frees up the loop
       loopCOunter = 0;
       input = "";
     }
 
-    //this is the arduino heart beat, if the arduino does not respond in this many loops, resend the request.
-    if(loopCOunter == 25){
+    // this is the arduino heart beat, if the arduino does not respond in this many loops, resend the request.
+    if (loopCOunter == 25)
+    {
       loopCOunter = 0;
       serialRequest = 0;
     }
-    if(Serial.available() == 0 && serialPState == false) loopCOunter++;
+    if (Serial.available() == 0 && serialPState == false)
+      loopCOunter++;
   }
 
-  else{
-    if(serialPRequest == 0) //Ask for ID
+  else
+  {
+    if (serialPRequest == 0) // Ask for ID
     {
       Serial.print("ID");
       serialRequest = 1;
     }
-    if(serialPRequest == 1 || serialPRequest == 7) //ASK for Volt1
+    if (serialPRequest == 1 || serialPRequest == 7) // ASK for Volt1
     {
       Serial.print("Volt1");
       serialRequest = 2;
     }
-    if(serialPRequest == 2) //ASK for Volt2
+    if (serialPRequest == 2) // ASK for Volt2
     {
       Serial.print("Volt2");
       serialRequest = 3;
     }
-    if(serialPRequest == 3) //ASK for Light1
+    if (serialPRequest == 3) // ASK for Light1
     {
       Serial.print("Light1");
       serialRequest = 4;
     }
-    if(serialPRequest == 4) //ASK for Light2
+    if (serialPRequest == 4) // ASK for Light2
     {
       Serial.print("Light2");
       serialRequest = 5;
     }
-    if(serialPRequest == 5) //ASK for Distance1
+    if (serialPRequest == 5) // ASK for Distance1
     {
       Serial.print("Distance1");
       serialRequest = 6;
     }
-    if(serialPRequest == 6) //ASK for Distance2
+    if (serialPRequest == 6) // ASK for Distance2
     {
       Serial.print("Distance2");
       serialRequest = 7;
@@ -141,6 +211,9 @@ void loop() {
  */
 void wifi_setup()
 {
+
+  delay(10);
+  espClient.setBufferSizes(512, 512);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
@@ -151,9 +224,9 @@ void wifi_setup()
   {
     Serial.print(".");
     if (timer % 16 == 0 && timer > 15)
-    Serial.printf("\n **Still trying to connect** , Timer: %i\n", timer);
+      Serial.printf("\n **Still trying to connect** , Timer: %i\n", timer);
     if (timer >= 64)
-    break;
+      break;
     timer++;
     delay(500);
   }
@@ -166,6 +239,13 @@ void wifi_setup()
   Serial.print("Connected, IP address:");
   Serial.print(WiFi.localIP());
   Serial.println();
+
+  timeClient.begin();
+  while (!timeClient.update())
+  {
+    timeClient.forceUpdate();
+  }
+  espClient.setX509Time(timeClient.getEpochTime());
 }
 
 /**
@@ -177,7 +257,7 @@ void MQTTreconnect()
   while (!MQTTclient.connected())
   {
     Serial.print("Attempting MQTT connection...");
-    String clientId = "ArduinoBox-";
+    String clientId = "ESPthing-";
     clientId += String(random(0xffff), HEX);
 
     if (MQTTclient.connect(clientId.c_str()))
@@ -191,6 +271,12 @@ void MQTTreconnect()
       Serial.print("failed, rc=");
       Serial.print(MQTTclient.state());
       Serial.println(" try again in 5 sec");
+
+      char buf[256];
+      espClient.getLastSSLError(buf, 256);
+      Serial.print("WiFiClientSecure SSL error: ");
+      Serial.println(buf);
+
       delay(5000);
     }
   }
@@ -199,7 +285,7 @@ void MQTTreconnect()
 /**
  * @brief Used to read messages from the subscribed topic.
  *        The ESP is not subscribed to any topics, but MQTT library requires this method
- * 
+ *
  * @param topic ESP would be subscribed to.
  * @param payload message posted to the topic.
  * @param length of message.
@@ -226,7 +312,9 @@ void MQTTinit()
   MQTTclient.setCallback(callback);
 }
 
-void mqttPost(String topic, String data){
-  if (!MQTTclient.connected()) MQTTreconnect();
-    MQTTclient.publish(topic.c_str(), data.c_str()); //data is sent to RPi
+void mqttPost(String topic, String data)
+{
+  if (!MQTTclient.connected())
+    MQTTreconnect();
+  MQTTclient.publish(topic.c_str(), data.c_str()); // data is sent to RPi
 }
